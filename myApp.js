05a008e646a9fa2c94acb1046f7ad644db4a62c6
IgorP <igorprzybysz999@gmail.com>
2022-05-11 17:55:30 +0200

    MongoDB and Mongoose

--- myApp.js
+++ myApp.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+const mongoose = require('mongoose');
 
 let Person;
 
@@ -50,6 +50,8 @@ const queryChain = (done) => {
 
   done(null /*, data*/);
 };
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
